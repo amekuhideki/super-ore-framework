9fde5e1dc203b3c116fb6fef24bbf6d3bcc11422
天久秀樹 <amekuhideki@amahisahidekinoMacBook-Pro.local>
2019-10-07 20:31:47 +0900

    htmlをphtmlに分離

--- app/routes.php
+++ app/routes.php
@@ -1,13 +1,9 @@
 <?php
 
-
 $routes = [];
 
 $routes['/'] = function () {
-  echo "<!DOCTYPE html>\n";
-  echo "<title>test</title>\n";
-  echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-  echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+  include __DIR__ . '/view/index.phtml';
   exit;
 };
 $routes['/phpinfo.php'] = function () {
