d6a5abac5eb133df9f1ef7e6344d918805c6df06
天久秀樹 <amekuhideki@amahisahidekinoMacBook-Pro.local>
2019-10-07 21:51:28 +0900

    use template function

--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,13 @@
 <?php
 
 $routes = [];
+$template = new \Oira\TemplateFactory(__DIR__ . '/view/');
 
-$routes['/'] = function () {
-  ob_start();
-  include __DIR__ . '/view/index.phtml';
-  
-  return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
+$routes['/'] = function () use ($template) {
+  return [200, ['Content-Type' => 'text/html'], $template->create('index', [
+    'title' => 'ameのホームページのindex',
+    'name' => 'ama'
+    ])];
 };
 $routes['/phpinfo.php'] = function () {
   phpinfo();
